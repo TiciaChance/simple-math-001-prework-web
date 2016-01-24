2fb4c0133a19c9c6c7cb9111eac21f53c3b0e0cd
TiciaChance <laticia.chance@gmail.com>
2016-01-24 13:16:34 -0500

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,27 @@
 def addition(num1, num2)
+  3+6
 end
 
 def subtraction(num1, num2)
+  7-2
 end
 
 def division(num1, num2)
+  100/4
 end
 
 def multiplication(num1, num2)
+  30*4
 end
 
 def modulo(num1, num2)
+  22%6
 end
 
 def square_root(num)
+  Math.sqrt(81)
 end
 
 def order_of_operation(num1, num2, num3, num4)
-  #Hint:  __ + (( __ * __ ) / __ )
+  15 + (( 7.5 * 2 ) / 5)
 end
